4a5a61e68877e305c53cc6706f61a9c07bc1c9df
Ernest <ernest.vila@cirvianum.cat>
2022-12-15 10:54:14 +0100

    primer commit amb l'estrucutra de l'array feta

---
+++ src/provacaixablancapractica1/ProvaCaixaBlancaPractica1.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package provacaixablancapractica1;
+
+/**
+ *
+ * @author ernes
+ */
+public class ProvaCaixaBlancaPractica1 {
+
+    public static void main(String[] args) {
+        String[][] cotxes = new String[7][4]; //Posicions: 0.Matricula, 1.Data Matriculacio, 2.Carburant, 3.
+
+        cotxes[0][0] = "";
+        cotxes[0][1] = "";
+        cotxes[0][2] = "";
+        cotxes[0][3] = "";
+        cotxes[1][0] = "";
+        cotxes[1][1] = "";
+        cotxes[1][2] = "";
+        cotxes[1][3] = "";
+        cotxes[2][0] = "";
+        cotxes[2][1] = "";
+        cotxes[2][2] = "";
+        cotxes[2][3] = "";
+        cotxes[3][0] = "";
+        cotxes[3][1] = "";
+        cotxes[3][2] = "";
+        cotxes[3][3] = "";
+        cotxes[4][0] = "";
+        cotxes[4][1] = "";
+        cotxes[4][2] = "";
+        cotxes[4][3] = "";
+        cotxes[5][0] = "";
+        cotxes[5][1] = "";
+        cotxes[5][2] = "";
+        cotxes[5][3] = "";
+        cotxes[6][0] = "";
+        cotxes[6][1] = "";
+        cotxes[6][2] = "";
+        cotxes[6][3] = "";
+        cotxes[7][0] = "";
+        cotxes[7][1] = "";
+        cotxes[7][2] = "";
+        cotxes[7][3] = "";
+        comprovarCotxes(cotxes);
+    }
+
+    private static void comprovarCotxes(String[][] cotxes) {
+
+    }
+
+}
